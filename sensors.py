3c510e008ebcaef094211a80f7760336d774477e
davistvmw <davist@vmware.com>
2018-09-25 17:59:29 +1200

    Added optimal_direction() function to sensors.py

--- sensors.py
+++ sensors.py
@@ -75,7 +75,7 @@ def optimal_direction():
     time.sleep(1)
     pwm.ChangeDutyCycle(10)
 ##Record distance on the left
-    print "distance on left =" front_distance()
+    print "distance on left =", front_distance()
     left_distance = front_distance()
 
 
@@ -83,7 +83,7 @@ def optimal_direction():
     time.sleep(1)
     pwm.ChangeDutyCycle(5)
     ##Record distance on the left
-    print "distance on right =" front_distance()
+    print "distance on right =", front_distance()
     right_distance = front_distance()
 
 
