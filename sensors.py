86369e58f9f5d4b0368e230ec2acb59badff3a3b
davistvmw <davist@vmware.com>
2018-09-25 17:44:31 +1200

    Added optimal_direction() function to sensors.py

--- sensors.py
+++ sensors.py
@@ -14,7 +14,97 @@ import time
 
 print("Distance Measurement in Progress")
 
-##Define a function to trigger the server to pan
+##Define the distance function (to be imported into drive script)
+def front_distance():
+
+##Define the GPIO pin number connected to trig
+    front_sensor_trig = 7
+##Define the GPIO pin number connected to echo
+    front_sensor_echo = 11
+
+##Set the gpio mode to "board" as opposed to BCM to use the physical pin numbers
+    gpio.setmode(gpio.BOARD)
+##Set up pins for trig (out of RPi, into sensor) and echo (out of sensor, in to RPi)
+    gpio.setup(front_sensor_trig, gpio.OUT)
+    gpio.setup(front_sensor_echo, gpio.IN)
+
+##Making sure that the output pin has no pre-configured value
+    gpio.output(front_sensor_trig, False)
+##Print out notice that the sensor is initiating
+##    print("Waiting for sensor to settle")
+##Give the sensor time to come online
+    time.sleep(0.001)
+
+##Trigger the sensor (8 ultrasound bursts at 40 kHz)
+    gpio.output(front_sensor_trig, True)
+##Confiture the length of the burst to 10uS
+    time.sleep(0.00001)
+##Stop the burst after 10uS
+    gpio.output(front_sensor_trig, False)
+
+##Listen on the echo pin and as long as there is no signal, take a time stamp (time.time())
+    while gpio.input(front_sensor_echo) == 0:
+        pulse_start = time.time()
+##Take a time stamp of the last recorded moment of a high signal
+    while gpio.input(front_sensor_echo) == 1:
+        pulse_end = time.time()
+##Pulse_duration is the time that passed between a signal appearing and disappearing
+    pulse_duration = pulse_end - pulse_start
+##The speed of sound in air at sea level = 343m/s or 34 300cm/s
+##s = d/t : d = s*t. The sound travels to the object and back so d = (s*t)/2
+    front_distance = 17150 * pulse_duration
+##Return an answer to 2 decimal places
+    front_distance = round(front_distance, 2)
+
+##Clen up the GPIO pins
+    gpio.cleanup()
+
+##Instruct the function to return 'distance'
+    return front_distance
+
+##Define a function to check the optimal direction for travel
+def optimal_direction():
+##Setup servo to pan
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(12,gpio.OUT)
+    pwm=gpio.PWM(12, 50)
+##Set starting position of servo
+    pwm.start(7.5)
+
+##Pan servo to the left
+    time.sleep(1)
+    pwm.ChangeDutyCycle(10)
+##Record distance on the left
+    left_distance = front_distance()
+    print "distance on left =" left_distance
+
+##Pan servo to the right
+    time.sleep(1)
+    pwm.ChangeDutyCycle(5)
+    ##Record distance on the left
+    right_distance = front_distance()
+    print "distance on right =" right_distance
+
+    #centre
+    time.sleep(1)
+    pwm.ChangeDutyCycle(7.5)
+    time.sleep(0.2)
+    gpio.cleanup()
+
+    if left_distance() < 20 and right_distance() < 20:
+        optimal_direction = 'reverse'
+    else:
+        dist_diff = left_distance() - right_distance()
+        if dist_diff >= 0:
+            optimal_direction = 'left'
+        if dist_diff <= 0:
+            optimal_direction = 'right'
+
+    return optimal_direction
+
+print optimal_direction()
+
+'''##Define a function to trigger the server to pan
 def front_pan():
 
     gpio.setmode(gpio.BOARD)
@@ -40,7 +130,8 @@ def front_pan():
     gpio.cleanup()
 
 ##front_pan()
-
+'''
+'''
 ##Define the distance function (to be imported into drive script)
 def front_distance():
 
@@ -90,7 +181,8 @@ def front_distance():
     return front_distance
 
 print front_distance(), "cm"
-
+'''
+'''
 ##Define the distance function for the back side (to be imported into drive script)
 def back_distance():
 ##Define the GPIO pin number xx connected to trig
@@ -140,7 +232,7 @@ def back_distance():
     return back_distance
 
 print back_distance(), "cm"
-
+'''
 '''
 ##Define the distance function for the right side (to be imported into drive script)
 def right_distance():
