6f09089c848bc08d79f1ec4fee26a2a820ccfdc0
davistvmw <davist@vmware.com>
2018-09-27 07:44:27 +1200

    separated driveme_test.py from driveme.py

---
+++ driveme_test.py
@@ -0,0 +1,35 @@
+##Using Python 2.7.3
+
+import RPi.GPIO as gpio
+import time
+import sys
+
+##Driveme.py Test Code:
+
+##Define the time to drive for (tf)
+tf = 3
+
+#drive vechicle forward for tf
+driveme.init()
+driveme.forward(tf)
+#turn vehicle left while moving forward for tf seconds
+driveme.init()
+driveme.reverse(tf)
+#turn vehicle left while moving forward for tf seconds
+driveme.init()
+driveme.turn_left_fwd(tf)
+#turn vehicle right while moving forward for tf seconds
+driveme.init()
+driveme.turn_right_fwd(tf)
+#turn vehicle left while reversing for tf seconds
+driveme.init()
+driveme.turn_left_rev(tf)
+#turn vehicle right while reversing for tf seconds
+driveme.init()
+driveme.turn_right_rev(tf)
+#Pivot vehicle clockwise (right) for tf seconds
+driveme.init()
+driveme.pivot_right(tf)
+#Pivot vehicle counterclockwise (left) while moving forward for tf seconds
+driveme.init()
+driveme.pivot_left(tf)
