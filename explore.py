d4623fd81dbfe9a63a0584566aca9ae513b576ff
davistvmw <davist@vmware.com>
2018-09-30 16:58:02 +1300

    Working on mode_discovery.py

--- explore.py
+++ explore.py
@@ -33,40 +33,111 @@ import random
 
 #Assumptions: see driveme.py
 
+def main():
+    print("Help! I'm a function without definition: define me!")
+##Once development is complete, move main() definition to the end of the script
+##Python Main if __name__ == "__main__" E.g. Set a function called "Start" at the start of the file - see guru99.com
+##def main():
+##    do stuff
+##Then put this in the end of the script. If python runs this file and you haven't specified anything it will run this..
+##if __name__ == "__main__":
+##    main()
 
-'''
-def check_rear():
-    r_dist = sensors.rear_distance()
-##Instruct action: if an object is closer than 10 cm away, check for the optimal direction and take evasive action
-    if r_dist <15
-##To be completed with opt_dir once this has been written. Meanwhile..
-    print "Too Close! Prepare to crash!"
-
-##Complete by adding in a response using optimal_direction
-    ##Consider putting in a random pivot right or left until optimal_direction function is complete
-<<<<<<< HEAD
-
-'''
-=======
-'''
-
->>>>>>> ebd939ba4195c5264cfd2190cd50d4e186f83ebb
-##Call the function front_distance from the local python script sensors.py
-##Define the function check_front to check the distance from the front sensor to the nearest object and respond
-##Best practice: Don't list if statements more than 2 deep, and try to nest 1 deep
-## Add in a die function:
-##def stop_program:
-##    exit(0)
+##Define a function to exit the program and print the reason "why" - to be specified in each function
+def abort_drive(why):
+    print(why)
+    exit(0)
 ## OR
-## switch to mode_RCD
+## switch to mode_RCD? Automatically? Or send a distress message?
 
-def check_front():
-##Define the variable f_dist as the distance from the front sensor to the nearest object
-    f_dist = sensors.front_distance()
-##Instruct action: if an object is closer than 15 cm away, check for the optimal direction and take evasive action
-    if f_dist < 15:
+##Define a function to return optimal direction for travel after checking the dictionary defined in sensors.py
+def optimal_direction():
+    print("Help! I'm a function wihtout a defininition - define me!")
 ##The optimal_direction function in sensors.py will return the best direction to move in 'right', 'left', or 'reverse'
 ##While the optimal_direction funciton is under development, this code is commented out
+
+##Define a function to check rear distance sensor by importing from the local python script sensors.py
+def check_rear():
+    try:
+##Define the variable r_dist as the distance from the rear sensor to the nearest object
+        r_dist = sensors.rear_distance()
+##Instruct action: if an object is closer than xx cm away, check for the optimal direction and take evasive action
+        if r_dist <15
+##To be completed with opt_dir once this has been written. Meanwhile..
+        print("Too Close! Prepare to crash!")
+    except:
+        print("If you had a sensor on the rear I'd check the distance now.")
+
+##Complete by adding in a response using optimal_direction
+##Consider putting in a random pivot right or left until optimal_direction function is complete
+##Best practice: Don't list if statements more than 2 deep, and try to nest 1 deep
+
+##Define a function to check rear distance sensor by importing from the local python script sensors.py
+def check_front():
+    try:
+##Define the variable f_dist as the distance from the front sensor to the nearest object
+        f_dist = sensors.front_distance()
+##Instruct action: if an object is closer than 15 cm away, check for the optimal direction and take evasive action
+        if f_dist < 15:
+##To be completed with opt_dir once this has been written. Meanwhile..
+        print "Too Close! Prepare to crash!"
+    except:
+        print("If you had a sensor on the front I'd check the distance now.")
+##Complete by adding in a response using optimal_direction
+##Best practice: Don't list if statements more than 2 deep, and try to nest 1 deep
+##May need to define check_front_2, check_front_3, check_front_4, etc.
+
+##Define a function to run as the primary vehicle autonomous drive mode: mode_discovery
+
+##Input: explore.mode_discovery(x, y, z)
+##x is the time that left or right forward drive will run for assuming no obstacles
+##y is the time that the vehicle should run for before checking input from the sensor
+##z is the bias 'left' or 'right'
+def mode_discovery(drive_time, drive_burst, mode):
+##When drive forward is chosen, continue for twice as long as the time spent driving to the left or right.
+    drive_iterate = int(round(drive_time / drive_burst))
+    drive_iterate_f = 2 * drive_iterate
+    ##Desired result is to drive forward drive_burst seconds before checking (e.g. 0.03)
+    ##Drive for drive_drive seconds
+    ##Drive_iterations = drive_time
+
+##1 represents forward, 2 represents forward and left, 3 represents forward and right
+    LHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
+    RHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 3, 3]
+##Set the bias right or left based on input chosen when calling mode_discovery(tf, mode)
+    if mode == 'left':
+        bias = LHB_options
+    elif mode == 'right':
+        bias = RHB_options
+    else:
+        print("'mode' defined in function mode_discovery(tf, mode) must be 'left' or 'right'. Please try again.")
+##Choose a random direction to travel in:
+    x = random.choice(bias)
+##If 1 is chosen at random from either LHB_options or RHB_options... (depending on bias 'left' or 'right')
+    if x == 1:
+##Repeat the steps below drive_iterate_f times
+        for y in range(drive_iterate_f):
+##Run the function check_front() to check the distance from the front sensor to the closest object
+            check_front()
+##Drive forward for drive_burst seconds
+            driveme.forward(drive_burst)
+##If 2 is chosen at random from either LHB_options or RHB_options... (depending on bias 'left' or 'right')
+    elif x == 2:
+##Repeat the steps below drive_iterate times
+        for y in range(drive_iterate):
+##Run the function check_front() to check the distance from the front sensor to the closest object
+            check_front()
+##Drive left and forward for drive_burst seconds
+            driveme.turn_left_fwd(drive_burst)
+##If 3 is chosen at random from either LHB_options or RHB_options... (depending on bias 'left' or 'right')
+    elif x == 3:
+        driveme.turn_right_fwd(drive_burst)
+    else:
+        abort_drive("Critical error in mode_discovery, selection of drive direction. Aborting...")
+
+##for z in range(10):
+##    mode_discovery(2, 'left')
+
 <<<<<<< HEAD
 '''        opt_dir = sensors.optimal_direction()
 =======
@@ -106,36 +177,8 @@ def check_front():
 '''
 ## If distance is >= 15cm, the script picks up after 'checkfront()'
 
-##Define a function to run as the primary vehicle autonomous drive mode: mode_discovery
-##Python Main if __name__ == "__main__" E.g. Set a function called "Start" at the start of the file - see guru99.com
-##def main():
-##    do stuff
-##Then put this in the end of the script. If python runs this file and you haven't specified anything it will run this..
-##if __name__ == "__main__":
-##    main()
-##Input: explore.mode_discovery(x, y)
-##x is the time that left or right forward drive will run for assuming no obstacles
-##y is the bias 'left' or 'right'
 
-def mode_discovery(tf, mode):
-#1 represents forward, 2 represents forward and left, 3 represents forward and right
-    front_tf = 2 * tf
-    LHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
-    RHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 3, 3]
-    if mode == 'left':
-        bias = LHB_options
-    elif mode == 'right':
-        bias = RHB_options
-    else:
-        print("Mode must be 'left' or 'right'. Please try again.")
 
-    x = random.choice(bias)
-    if x == 1:
-        driveme.forward(front_tf)
-    if x == 2:
-        driveme.turn_right_fwd(tf)
-    else:
-        driveme.turn_left_fwd(tf)
 '''
 def mode_discovery_no_obstacles(tf, mode):
 #1 represents forward, 2 represents forward and left, 3 represents forward and right
