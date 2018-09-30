bb92e7efa95ef85d17284980435c4d700a39f382
davistvmw <davist@vmware.com>
2018-09-30 13:45:22 +1300

    added printed statements to driveme_test.py

--- driveme_test.py
+++ driveme_test.py
@@ -12,17 +12,24 @@ tf = 3
 
 #drive vechicle forward for tf
 driveme.forward(tf)
+print("driving forward")
 #turn vehicle left while moving forward for tf seconds
 driveme.reverse(tf)
+print("driving in reverse")
 #turn vehicle left while moving forward for tf seconds
 driveme.turn_left_fwd(tf)
+print("driving forward and left")
 #turn vehicle right while moving forward for tf seconds
 driveme.turn_right_fwd(tf)
+print("driving forward and right")
 #turn vehicle left while reversing for tf seconds
 driveme.turn_left_rev(tf)
+print("reversing to the left")
 #turn vehicle right while reversing for tf seconds
 driveme.turn_right_rev(tf)
+print("reversing to the right")
 #Pivot vehicle clockwise (right) for tf seconds
 driveme.pivot_right(tf)
+print("reversing to the right")
 #Pivot vehicle counterclockwise (left) while moving forward for tf seconds
 driveme.pivot_left(tf)
