2b49772a1e2b8e537f912a19246911d117beeeb4
davistvmw <davist@vmware.com>
2018-10-02 14:53:36 +1000

    put explore in development mode

--- explore.py
+++ explore.py
@@ -53,17 +53,17 @@ def abort_drive(why):
 ## OR
 ## switch to mode_RCD? Automatically? Or send a distress message?
 
-#Removed optimal_direction???
-
 ##Define a function to check rear distance sensor by importing from the local python script sensors.py
 def check_rear():
     try:
 ##Define the variable r_dist as the distance from the rear sensor to the nearest object
         r_dist = sensors.rear_distance()
 ##Instruct action: if an object is closer than xx cm away, check for the optimal direction and take evasive action
-        if r_dist <15
+        if r_dist <15:
 ##To be completed with opt_dir once this has been written. Meanwhile..
-        print("Too Close! Prepare to crash!")
+            print("Too Close! Prepare to crash!")
+        else:
+            print("All clear at the back!")
     except:
         print("If you had a sensor on the rear I'd check the distance now.")
 
@@ -79,7 +79,11 @@ def check_front():
 ##Instruct action: if an object is closer than 15 cm away, check for the optimal direction and take evasive action
         if f_dist < 15:
 ##To be completed with opt_dir once this has been written. Meanwhile..
-        print "Too Close! Prepare to crash!"
+            print("Too Close! Prepare to crash!")
+
+        else:
+            print("All clear in front!")
+
     except:
         print("If you had a sensor on the front I'd check the distance now.")
 ##Complete by adding in a response using optimal_direction
@@ -141,161 +145,3 @@ def mode_discovery(drive_time, drive_burst, mode):
 
 ##for z in range(10):
 ##    mode_discovery(2, 'left')
-
-<<<<<<< HEAD
-'''        opt_dir = sensors.optimal_direction()
-=======
-        opt_dir = sensors.optimal_direction()
->>>>>>> ebd939ba4195c5264cfd2190cd50d4e186f83ebb
-        if opt_dir == 'left':
-            for y in range(30):
-                ##check_rear()
-                driveme.reverse(0.03)
-            driveme.turn_left_fwd(2)
-            ##what about checking the front again while driving foward?
-        elif opt_dir == 'right':
-            for y in range(30):
-                ##check_rear()
-                driveme.reverse(0.03)
-            driveme.turn_right_fwd(2)
-            ##what about checking the front again while driving foward?
-        elif opt_dir == 'reverse':
-
-        else opt_dir = 'left':
-            driveme.init()
-            for y in range(30):
-                check_rear()
-                driveme.init()
-                driveme.reverse(0.03)
-            '''
-
-'''##Temporary instructions for behavior to be performed while optimal_direction function is under development
-##NEXT: Update reverse to include a rear camera check check_rear
-        driveme.init()
-        driveme.reverse(2)
-
-        for y in range(30):
-            check_rear()
-            driveme.init()
-            driveme.reverse(0.03)
-'''
-## If distance is >= 15cm, the script picks up after 'checkfront()'
-
-
-
-'''
-def mode_discovery_no_obstacles(tf, mode):
-#1 represents forward, 2 represents forward and left, 3 represents forward and right
-    front_tf = 2 * tf
-    LHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 2, 3]
-    RHB_options = [1, 1, 1, 1, 1, 1, 1, 2, 3, 3]
-    if mode == 'left':
-        bias = LHB_options
-    elif mode == 'right':
-        bias = RHB_options
-    else:
-        print("Mode must be 'left' or 'right'. Please try again.")
-
-    x = random.choice(bias)
-    if x == 1:
-        driveme.forward(front_tf)
-    if x == 2:
-        driveme.turn_right_fwd(tf)
-    else:
-        driveme.turn_left_fwd(tf)
-'''
-
-'''for z in range(10):
-    mode_discovery(2, 'left')'''
-
-
-'''
-        ##Repeat the steps below 30 times
-            for y in range(60):
-                check_front()
-        ##Initialise GPIO pins (based on instructions defined in driveme.py)
-                driveme.init()
-        ##Drive forward for 0.03 seconds
-                driveme.forward(tf)
-
-    if bias == 'right':
-        xxx
-'''
-
-
-
-
-
-'''
-##Define the function autonomy
-##NEXT: Program in left hand bias and right hand bias
-def autonomy():
-##Set the time to run (for actions other than forward)
-    tf = 0.03
-##Introduce a variable, x, that will take on a sudorandom value to drive the vechicle in explore mode. x will take on values from 1-7
-    x = random.randrange(0, 7)
-##Set actions for the vechicle based on the value of x
-
-##Drive forward for 5 seconds if x = 0
-    if x == 0:
-##Repeat the steps below 30 times
-        for y in range(60):
-            check_front()
-##Initialise GPIO pins (based on instructions defined in driveme.py)
-            driveme.init()
-##Drive forward for 0.03 seconds
-            driveme.forward(tf)
-
-##Pivot left for tf seconds if x = 1
-    elif x == 1:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.pivot_left(tf)
-
-##Pivot right for tf seconds if x = 2
-    elif x == 2:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.pivot_right(tf)
-
-##Drive forward and to the left for tf seconds if x = 3
-    elif x == 3:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.turn_left_fwd(tf)
-
-##Drive forward and to the right for tf seconds if x = 4
-    elif x == 4:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.turn_right_fwd(tf)
-
-##Drive left and reverse for tf seconds if x = 5
-    elif x == 5:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.turn_left_rev(tf)
-
-##Drive right and reverse for tf seconds if x = 6
-    elif x == 6:
-        for y in range(30):
-            check_front()
-            driveme.init()
-            driveme.turn_right_rev(tf)
-'''
-'''##Reverse for tf seconds if x = 7
-    elif x == 7:
-        for y in range(10):
-            check_front()
-            driveme.init()
-            driveme.reverse(tf)
-
-##Run the function autonomy 10 times (generate 10 random iterations of x which will produce 10 movements at random)
-for z in range(10):
-    autonomy()
-'''
