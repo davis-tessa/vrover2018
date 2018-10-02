4c26a70403c77ddc5e8ea481d6d2845ed2da27c9
davistvmw <davist@vmware.com>
2018-10-02 17:31:25 +1000

    updated explore.py to map to driveme_tank.py

--- explore.py
+++ explore.py
@@ -8,8 +8,8 @@ import time
 import sys
 ##Import local library python-sensors: see sensors.py
 import sensors
-##Import local library python-driveme: see driveme.py
-import driveme
+##Import local library python-driveme_tank: see driveme_tank.py
+import driveme_tank
 ##Import public library python-random
 import random
 ##Import local library optimal_direction: see optimal_direction.py
@@ -23,17 +23,17 @@ import optimal_direction
 ##1. Drive vechicle autonomously in "explore" mode.
 ##2. Constantly check the distance on the front sensor, print the result and take an additional remediation action
 # ---sensors.distance() --- Return the distance from the sensor to the nearest object
-## ---driveme.init() --- Initialise GPIO pins to drive as output
-## ---driveme.forward(tf) --- Drive Foward
-## ---driveme.reverse(tf) --- Drive in Reverse
-## ---driveme.turn_left_fwd(tf) --- Turn left while moving forward
-## ---driveme.turn_right_fwd(tf) --- Turn right while moving forward
-## ---driveme.turn_left_rev(tf) --- Turn left while moving backward
-## ---driveme.turn_right_rev(tf) --- Turn right while moving backward
-## ---driveme.pivot_right(tf) --- Pivot clockwise (defined as from a 'birds eye view' with 12o'clock at the front of the vehicle (Pivot right)
-## ---driveme.pivot_left(tf) --- Pivot counter clockwise (Pivot left)
+## ---driveme_tank.init() --- Initialise GPIO pins to drive as output
+## ---driveme_tank.forward(tf) --- Drive Foward
+## ---driveme_tank.reverse(tf) --- Drive in Reverse
+## ---driveme_tank.turn_left_fwd(tf) --- Turn left while moving forward
+## ---driveme_tank.turn_right_fwd(tf) --- Turn right while moving forward
+## ---driveme_tank.turn_left_rev(tf) --- Turn left while moving backward
+## ---driveme_tank.turn_right_rev(tf) --- Turn right while moving backward
+## ---driveme_tank.pivot_right(tf) --- Pivot clockwise (defined as from a 'birds eye view' with 12o'clock at the front of the vehicle (Pivot right)
+## ---driveme_tank.pivot_left(tf) --- Pivot counter clockwise (Pivot left)
 
-#Assumptions: see driveme.py
+#Assumptions: see driveme_tank.py
 
 def main():
     print("Help! I'm a function without definition: define me!")
@@ -124,7 +124,7 @@ def mode_discovery(drive_time, drive_burst, mode):
 ##Run the function check_front() to check the distance from the front sensor to the closest object
             check_front()
 ##Drive forward for drive_burst seconds
-            driveme.forward(drive_burst)
+            driveme_tank.forward(drive_burst)
 ##If 2 is chosen at random from either LHB_options or RHB_options... (depending on bias 'left' or 'right')
     elif x == 2:
 ##Repeat the steps below drive_iterate times
@@ -132,7 +132,7 @@ def mode_discovery(drive_time, drive_burst, mode):
 ##Run the function check_front() to check the distance from the front sensor to the closest object
             check_front()
 ##Drive left and forward for drive_burst seconds
-            driveme.turn_left_fwd(drive_burst)
+            driveme_tank.turn_left_fwd(drive_burst)
 ##If 3 is chosen at random from either LHB_options or RHB_options... (depending on bias 'left' or 'right')
     elif x == 3:
 ##Repeat the steps below drive_iterate times
@@ -140,7 +140,7 @@ def mode_discovery(drive_time, drive_burst, mode):
 ##Run the function check_front() to check the distance from the front sensor to the closest object
             check_front()
 ##Drive left and forward for drive_burst seconds
-            driveme.turn_right_fwd(drive_burst)
+            driveme_tank.turn_right_fwd(drive_burst)
     else:
         abort_drive("Critical error in mode_discovery. Aborting...")
 
