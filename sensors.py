0369138346d7999da617f5abd4cbad5fa6181f96
davistvmw <davist@vmware.com>
2018-10-12 17:06:38 +1300

    calibrate front sensor set

--- sensors.py
+++ sensors.py
@@ -43,7 +43,7 @@ def front_distance():
         ##Print out notice that the sensor is initiating
         ##    print("Waiting for sensor to settle")
         ##Give the sensor time to come online
-        time.sleep(0.001)
+        time.sleep(0.01)
 
         ##Trigger the sensor (8 ultrasound bursts at 40 kHz)
         gpio.output(front_sensor_trig, True)
@@ -106,7 +106,7 @@ def rear_distance():
     ##Print out notice that the sensor is initiating
     ##    print("Waiting for sensor to settle")
     ##Give the sensor time to come online
-        time.sleep(0.001)
+        time.sleep(0.01)
 
     ##Trigger the sensor (8 ultrasound bursts at 40 kHz)
         gpio.output(rear_sensor_trig, True)
