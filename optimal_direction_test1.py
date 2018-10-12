0c0468132d668dffd77dc564c37ba1ff4a6175de
davistvmw <davist@vmware.com>
2018-10-12 16:04:15 +1300

    adding seg_fault_test

--- optimal_direction_test1.py
+++ optimal_direction_test1.py
@@ -5,4 +5,5 @@ import sys
 import optimal_direction
 
 ##Test: Return the optimal_direction based on running optimal_direction.py
-optimal_direction.optimal_direction()
+opt_dir = optimal_direction.optimal_direction()
+print("opt_dir = ", opt_dir)
