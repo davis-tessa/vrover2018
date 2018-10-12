ca1228815ca02b96d41bb15bd1fbd4ad7e9447f2
davistvmw <davist@vmware.com>
2018-10-12 14:41:19 +1300

    Preparing for forward evade tests

--- evade_fwd_test2.py
+++ evade_fwd_test2.py
@@ -7,4 +7,4 @@ import evade
 
 ## Test: Test top level evade logic - change x to highest level evade_fwd_x used in script
 print("\n\nStarting Test: evade_fwd_test2\n\n")
-evade.evade_fwd_3(1, 1)
+evade.evade_fwd_3(2, 2)
