6e92f8084340888f23452ad5d5e9c17c73f633b2
davistvmw <davist@vmware.com>
2018-10-12 14:57:21 +1300

    Preparing for reverse evade tests

--- evade_rev_test1.py
+++ evade_rev_test1.py
@@ -1,20 +1,8 @@
-##Using Python 2.7.3
+##Using Python 3.6.5
 
-##Import public library python-RPi.GPIO
-import RPi.GPIO as gpio
-##Import public library python-time
-import time
 ##Import public library python-sys
 import sys
 ##Import local library python-sensors: see sensors.py
-import sensors
-##Import local library python-driveme_tank: see driveme_tank.py
-import driveme_tank
-##Import public library python-random
-import random
-##Import local library optimal_direction: see optimal_direction.py
-import optimal_direction
-##Import local library evade
 import evade
 
 ## Test: Test top level check_bk logic - change x to highest level check_bk_x used in script
