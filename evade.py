1e6ede102835d10995a3e90bd05113e13b2e2d2c
davistvmw <davist@vmware.com>
2018-10-16 09:03:46 +1100

    Adding Pivot to evade code

--- evade.py
+++ evade.py
@@ -254,6 +254,8 @@ def evade_fwd_1(drive_time, drive_burst, mode):
         print("evade.evade_fwd_1  > I will proceed forward and left with caution")
         ##Repeat the steps below drive_iterate times
         for y in range(drive_iterate):
+            print("evade.evade_fwd_1  > Pivoting to the left")
+            driveme_tank.pivot_left(drive_time, mode)
             ##Run the function collision_avoidance() to check the distance from the front sensor to the closest object
             check_fr_2(drive_time, drive_burst, mode)
             ##Drive left and forward for drive_burst seconds
@@ -263,6 +265,7 @@ def evade_fwd_1(drive_time, drive_burst, mode):
         print("evade.evade_fwd_1  > I will proceed forward and right with caution")
         ##Repeat the steps below drive_iterate times
         for y in range(drive_iterate):
+            print("evade.evade_fwd_1  > Pivoting to the right")
             driveme_tank.pivot_right(drive_time, mode)
             ##Run the function collision_avoidance() to check the distance from the front sensor to the closest object
             check_fr_2(drive_time, drive_burst, mode)
@@ -272,7 +275,6 @@ def evade_fwd_1(drive_time, drive_burst, mode):
     elif opt_dir == 'reverse':
         ##Repeat the steps below drive_iterate times
         for y in range(drive_iterate):
-            driveme_tank.pivot_left(drive_time, mode)
             ##Run the function collision_avoidance() to check the distance from the front sensor to the closest object
             check_bk_1(drive_time, drive_burst, mode)
             ##Drive left and forward for drive_burst seconds
