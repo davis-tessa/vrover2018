d9e4e48405157d9a41c6702be6237dc78bbfd22e
Kevin Donnelly <kdonnelly@vmware.com>
2018-10-16 17:43:51 +1100

    extended sensor time on drive_left.py for longer distance at luna park off

--- drive_left.py
+++ drive_left.py
@@ -9,6 +9,6 @@ gpio_cleanup.cleanup()
 print("\n\ndrive_left         > Starting: drive_left\n\n")
 
 for z in range(20):
-    explore.mode_discovery(1, 0.10, 'left', 'on')
+    explore.mode_discovery(1, 0.10, 'left', 'off')
 
 print("\n\ndrive_left         > Finished drive_left\n\nrun me again!")
