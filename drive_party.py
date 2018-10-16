b82ddb627af61f19eae174b8961cd623663307ba
Kevin Donnelly <kdonnelly@vmware.com>
2018-10-16 18:42:44 +1100

    extended sensor time on drive_party.py for longer distance at luna park 0.8

--- drive_party.py
+++ drive_party.py
@@ -7,6 +7,6 @@ gpio_cleanup.cleanup()
 print("\n\nStarting Test: drive_party\n\n")
 
 for z in range(20):
-    explore.mode_discovery(2, 0.03, 'party', 'on')
+    explore.mode_discovery(2, 0.85, 'party', 'on')
 
 print("\n\nFinished drive_party\n\nrun me again!")
