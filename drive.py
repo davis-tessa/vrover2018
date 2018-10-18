b5401738143ab49fa56f3eb154db2b8b9a26dedb
kdawg5 <kdonnelly@vmware.com>
2018-10-18 14:35:16 +1100

    git update rpi onboard

--- drive.py
+++ drive.py
@@ -1,10 +1,11 @@
 
 ##Import local library explore: see explore.py
+
 import explore
 
-print("\n\nStarting Test: drive_party\n\n")
+print("\n\nStarting Test: drive_right\n\n")
 
 for z in range(20):
-    explore.mode_discovery(2, 2.00, 'party', 'on')
+    explore.mode_discovery(2, 0.85, 'right', 'on')
 
-print("\n\nFinished drive_party\n\nrun me again!")
+print("\n\nFinished drive_right\n\nrun me again!")
